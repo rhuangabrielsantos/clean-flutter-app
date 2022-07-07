6a33fd4cf8bc7ee2ca970b6a08c564a6105a759d
Rhuan Gabriel <rhuan.santos@db1.com.br>
2022-07-07 20:20:17 -0300

    :test_tube: Ensure RemoteAuthentication throws UnexpectedError on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -47,4 +47,16 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Shoud throw UnexpectedError if HttpClient return 404', () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body')
+    )).thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
