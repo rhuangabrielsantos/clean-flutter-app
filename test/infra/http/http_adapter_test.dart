d3bd600164ea4b5d8df628e623389d9214608a90
Rhuan Gabriel <rhuan.santos@db1.com.br>
2022-07-12 09:29:44 -0300

    :test_tube: Ensure HttpAdater calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -9,13 +11,17 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required String url, @required String method}) async {
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
     final uri = Uri.parse(url);
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(uri, headers: headers);
+    await client.post(uri, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -36,12 +42,20 @@ void main() {
 
   group('POST', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'POST');
+      await sut.request(
+        url: url,
+        method: 'POST',
+        body: {'any_key': 'any_value'},
+      );
 
-      verify(client.post(uri, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json',
-      }));
+      verify(client.post(
+        uri,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+        body: '{"any_key":"any_value"}',
+      ));
     });
   });
 }
