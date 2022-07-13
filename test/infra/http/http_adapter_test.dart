ef707c0a79845f790e35a4a7061874fa9a7691e8
Rhuan Gabriel <rhuan.santos@db1.com.br>
2022-07-12 21:39:03 -0300

    :test_tube: Ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -42,6 +42,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -141,6 +145,14 @@ void main() {
 
     test('Should return SeverError if post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return SeverError if post throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
